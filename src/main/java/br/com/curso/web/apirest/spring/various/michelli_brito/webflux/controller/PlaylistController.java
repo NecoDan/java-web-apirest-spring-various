2b960d5887dd15967d9d33798d37744cfdc6fbe4
Daniel Santos <neco.daniel@gmail.com>
2021-03-03 03:55:59 -0300

    implementacao dos controllers

--- src/main/java/br/com/curso/web/apirest/spring/various/michelli_brito/webflux/controller/PlaylistController.java
+++ src/main/java/br/com/curso/web/apirest/spring/various/michelli_brito/webflux/controller/PlaylistController.java
@@ -1,9 +1,31 @@
 package br.com.curso.web.apirest.spring.various.michelli_brito.webflux.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.curso.web.apirest.spring.various.michelli_brito.webflux.model.Playlist;
+import br.com.curso.web.apirest.spring.various.michelli_brito.webflux.service.IPlaylistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping
+@RequestMapping("/v1/playlists")
+@RequiredArgsConstructor
 public class PlaylistController {
+
+    private final IPlaylistService playlistService;
+
+    @GetMapping
+    public Flux<Playlist> getAll() {
+        return this.playlistService.findAll();
+    }
+
+    @GetMapping(value = ("/{id}"))
+    public Mono<Playlist> getById(@PathVariable String id) {
+        return this.playlistService.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Playlist> save(@RequestBody Playlist playlist) {
+        return this.playlistService.save(playlist);
+    }
 }
--- src/main/java/br/com/curso/web/apirest/spring/various/michelli_brito/webflux/service/IPlaylistService.java
+++ src/main/java/br/com/curso/web/apirest/spring/various/michelli_brito/webflux/service/IPlaylistService.java
@@ -1,5 +1,15 @@
 package br.com.curso.web.apirest.spring.various.michelli_brito.webflux.service;
 
+import br.com.curso.web.apirest.spring.various.michelli_brito.webflux.model.Playlist;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface IPlaylistService {
+
+    Flux<Playlist> findAll();
+
+    Mono<Playlist> findById(String id);
+
+    Mono<Playlist> save(Playlist playlist);
 
 }
